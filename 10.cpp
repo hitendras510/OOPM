146d2025d6a7fb244315833a8336d6a29b7efaf0
Hitendra singh <hitendrasingh5489@gmail.com>
2025-11-25 10:44:17 +0530

    . 

--- 10.cpp
+++ 10.cpp
@@ -1,2 +1,29 @@
 #include<iostream>;
 using namespace std;
+
+class Appliance{
+    public:
+    virtual void start(){
+        cout<<"Generic Appliance\n"<<endl;
+    }
+};
+
+class Toaster: public Appliance{
+    public:
+    void start(){
+        cout<<"Toaster calls\n"<<endl;
+    }
+};
+
+class Microwave: public Appliance{
+    public:
+    void start(){
+        cout<<"Microwave calls\n"<<endl;
+    }
+};
+
+int main()
+{
+
+    
+}
